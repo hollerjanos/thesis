57958d0f7edcd429b1e5e360577899e09b252322
Holler János <hollerjanika@gmail.com>
2023-11-06 00:44:20 +0100

    Few updates and new files
    Trying to get used to PSR-2 Coding Style.
    Added new files to test and style.

--- includes/classes/Database.php
+++ includes/classes/Database.php
@@ -10,11 +10,24 @@
 // Editor: PhpStorm 2022.2.3
 
 //==============================================================================
+// Namespace
+//==============================================================================
+
+namespace includes\classes;
+
+//==============================================================================
 // Includes
 //==============================================================================
 
 // Functions
-require_once($_SERVER["DOCUMENT_ROOT"]."/includes/functions.php");
+require_once("../functions.php");
+
+//==============================================================================
+// Imports
+//==============================================================================
+
+use PDO;
+use Exception;
 
 //==============================================================================
 // Process
@@ -22,262 +35,265 @@ require_once($_SERVER["DOCUMENT_ROOT"]."/includes/functions.php");
 
 class Database
 {
-  //============================================================================
-  // Properties
-  //============================================================================
+    //============================================================================
+    // Properties
+    //============================================================================
 
-  private string $hostname;
-  private string $username;
-  private string $password;
-  private string $database;
+    private string $hostname;
+    private string $username;
+    private string $password;
 
-  private PDO $db;
+    private PDO $db;
 
-  private bool $debug = true;
+    private bool $debug = true;
 
-  //============================================================================
-  // Construct
-  //============================================================================
+    //============================================================================
+    // Construct
+    //============================================================================
 
-  public function __construct(
-    string $hostname = "localhost",
-    string $username = "root",
-    string $password = "",
-    string $database = "2fa"
-  )
-  {
-    $this->hostname = $hostname;
-    $this->username = $username;
-    $this->password = $password;
-    $this->database = $database;
-
-    $this->connection();
-
-    $this->setAttributes();
-  }
-
-  /**
-   * <p>Database connection</p>
-   * @return void
-   */
-  private function connection(): void
-  {
-    // Data source name
-    $dsn = "mysql:host=$this->hostname";
-
-    $this->db = new PDO($dsn, $this->username, $this->password);
-  }
-
-  /**
-   * <p>Set attributes</p>
-   * @return void
-   */
-  private function setAttributes(): void
-  {
-    $this->db->setAttribute(
-      PDO::ATTR_ERRMODE,
-      PDO::ERRMODE_EXCEPTION
-    );
-    $this->db->setAttribute(
-      PDO::ATTR_DEFAULT_FETCH_MODE,
-      PDO::FETCH_ASSOC
-    );
-  }
-
-  /**
-   * <p>Create database</p>
-   * @return bool
-   */
-  public function createDatabase(): bool
-  {
-    try
+    public function __construct(
+        string $hostname = "localhost",
+        string $username = "root",
+        string $password = ""
+    )
     {
-      $sql = "CREATE DATABASE IF NOT EXISTS `$this->database`;";
+        $this->hostname = $hostname;
+        $this->username = $username;
+        $this->password = $password;
 
-      if ($this->debug)
-        display(
-          "Database | createDatabase()",
-          [
-            "sql" => $sql
-          ]
-        );
+        $this->connection();
 
-      $statement = $this->db->prepare($sql);
-      $statement->execute();
-
-      return true;
+        $this->setAttributes();
     }
-    catch (Exception $exception)
+
+    /**
+     * <p>Database connection</p>
+     * @return void
+     */
+    private function connection(): void
     {
-      if ($this->debug)
-        exception(
-          "Database | createDatabase()",
-          [
-            "message" => $exception->getMessage()
-          ]
-        );
+        // Data source name
+        $dsn = "mysql:host=$this->hostname";
 
-      return false;
+        $this->db = new PDO($dsn, $this->username, $this->password);
     }
-  }
 
-  /**
-   * <p>Select database</p>
-   * @return bool
-   */
-  public function selectDatabase(): bool
-  {
-    try
+    /**
+     * <p>Set attributes</p>
+     * @return void
+     */
+    private function setAttributes(): void
     {
-      $sql = "USE `$this->database`;";
-
-      if ($this->debug)
-        display(
-          "Database | selectDatabase()",
-          [
-            "sql" => $sql
-          ]
+        $this->db->setAttribute(
+            PDO::ATTR_ERRMODE,
+            PDO::ERRMODE_EXCEPTION
         );
-
-      $statement = $this->db->prepare($sql);
-      $statement->execute();
-
-      return true;
+        $this->db->setAttribute(
+            PDO::ATTR_DEFAULT_FETCH_MODE,
+            PDO::FETCH_ASSOC
+        );
     }
-    catch (Exception $exception)
+
+    /**
+     * <p>Create database</p>
+     * @param string $database
+     * @return bool
+     */
+    public function createDatabase(
+        string $database
+    ): bool {
+        try
+        {
+            $sql = "CREATE DATABASE IF NOT EXISTS `$database`;";
+
+            if ($this->debug)
+                display(
+                    "Database | createDatabase()",
+                    [
+                        "sql" => $sql
+                    ]
+                );
+
+            $statement = $this->db->prepare($sql);
+            $statement->execute();
+
+            return true;
+        }
+        catch (Exception $exception)
+        {
+            if ($this->debug)
+                exception(
+                    "Database | createDatabase()",
+                    [
+                        "message" => $exception->getMessage()
+                    ]
+                );
+
+            return false;
+        }
+    }
+
+    /**
+     * <p>Select database</p>
+     * @param string $database
+     * @return bool
+     */
+    public function selectDatabase(
+        string $database
+    ): bool {
+        try
+        {
+            $sql = "USE `$database`;";
+
+            if ($this->debug)
+                display(
+                    "Database | selectDatabase()",
+                    [
+                        "sql" => $sql
+                    ]
+                );
+
+            $statement = $this->db->prepare($sql);
+            $statement->execute();
+
+            return true;
+        }
+        catch (Exception $exception)
+        {
+            if ($this->debug)
+                exception(
+                    "Database | selectDatabase()",
+                    [
+                        "message" => $exception->getMessage()
+                    ]
+                );
+
+            return false;
+        }
+    }
+
+    /**
+     * <p>Create table</p>
+     * @param string $name
+     * @param array $config
+     * @return bool
+     */
+    public function createTable(string $name, array $config): bool
     {
-      if ($this->debug)
-        exception(
-          "Database | selectDatabase()",
-          [
-            "message" => $exception->getMessage()
-          ]
-        );
+        try
+        {
+            $attributes = [];
+            foreach ($config as $key => $item)
+                $attributes[] = "`$key` $item";
+            $attributes = implode(", ", $attributes);
 
-      return false;
+            $sql  = "CREATE TABLE IF NOT EXISTS `$name` ($attributes);";
+
+            if ($this->debug)
+                display(
+                    "Database | createTable()",
+                    [
+                        "sql" => $sql,
+                        "config" => $config
+                    ]
+                );
+
+            $statement = $this->db->prepare($sql);
+            $statement->execute();
+
+            return true;
+        }
+        catch (Exception $exception)
+        {
+            if ($this->debug)
+                exception(
+                    "Database | createTable()",
+                    $exception->getMessage()
+                );
+
+            return false;
+        }
     }
-  }
 
-  /**
-   * <p>Create table</p>
-   * @param string $name
-   * @param array $config
-   * @return bool
-   */
-  public function createTable(string $name, array $config): bool
-  {
-    try
+    /**
+     * <p>Insert into table</p>
+     * @param string $name
+     * @param $data
+     * @return bool
+     */
+    public function insertIntoTable(string $name, $data): bool
     {
-      $attributes = [];
-      foreach ($config as $key => $item)
-        $attributes[] = "`$key` $item";
-      $attributes = implode(", ", $attributes);
+        try
+        {
+            $keys = "`" . implode("`, `", array_keys($data)) . "`";
+            $items = ":" . implode(", :", array_keys($data));
 
-      $sql  = "CREATE TABLE IF NOT EXISTS `$name` ($attributes);";
+            $sql  = "INSERT INTO `$name` ($keys) VALUES ($items);";
 
-      if ($this->debug)
-        display(
-          "Database | createTable()",
-          [
-            "sql" => $sql,
-            "config" => $config
-          ]
-        );
+            if ($this->debug)
+                display(
+                    "Database | insertIntoTable()",
+                    [
+                        "sql" => $sql,
+                        "data" => $data
+                    ]
+                );
 
-      $statement = $this->db->prepare($sql);
-      $statement->execute();
+            $statement = $this->db->prepare($sql);
+            $statement->execute($data);
 
-      return true;
+            return true;
+        }
+        catch (Exception $exception)
+        {
+            if ($this->debug)
+                exception(
+                    "Database | insertIntoTable()",
+                    [
+                        "message" => $exception->getMessage()
+                    ]
+                );
+
+            return false;
+        }
     }
-    catch (Exception $exception)
-    {
-      if ($this->debug)
-        exception(
-          "Database | createTable()",
-          $exception->getMessage()
-        );
 
-      return false;
+    /**
+     * <p>Delete database</p>
+     * @param string $database
+     * @return bool
+     */
+    public function deleteDatabase(
+        string $database
+    ): bool {
+        try
+        {
+            $sql = "DROP DATABASE IF EXISTS `$database`;";
+
+            if ($this->debug)
+                display(
+                    "Database | deleteDatabase()",
+                    [
+                        "sql" => $sql
+                    ]
+                );
+
+            $statement = $this->db->prepare($sql);
+            $statement->execute();
+
+            return true;
+        }
+        catch (Exception $exception)
+        {
+            if ($this->debug)
+                exception(
+                    "Database | deleteDatabase()",
+                    [
+                        "message" => $exception->getMessage()
+                    ]
+                );
+
+            return false;
+        }
     }
-  }
-
-  /**
-   * <p>Insert into table</p>
-   * @param string $name
-   * @param $data
-   * @return bool
-   */
-  public function insertIntoTable(string $name, $data): bool
-  {
-    try
-    {
-      $keys = "`" . implode("`, `", array_keys($data)) . "`";
-      $items = ":" . implode(", :", array_keys($data));
-
-      $sql  = "INSERT INTO `$name` ($keys) VALUES ($items);";
-
-      if ($this->debug)
-        display(
-          "Database | insertIntoTable()",
-          [
-            "sql" => $sql,
-            "data" => $data
-          ]
-        );
-
-      $statement = $this->db->prepare($sql);
-      $statement->execute($data);
-
-      return true;
-    }
-    catch (Exception $exception)
-    {
-      if ($this->debug)
-        exception(
-          "Database | insertIntoTable()",
-          [
-            "message" => $exception->getMessage()
-          ]
-        );
-
-      return false;
-    }
-  }
-
-  /**
-   * <p>Delete database</p>
-   * @return bool
-   */
-  public function deleteDatabase(): bool
-  {
-    try
-    {
-      $sql = "DROP DATABASE IF EXISTS `$this->database`;";
-
-      if ($this->debug)
-        display(
-          "Database | deleteDatabase()",
-          [
-            "sql" => $sql
-          ]
-        );
-
-      $statement = $this->db->prepare($sql);
-      $statement->execute();
-
-      return true;
-    }
-    catch (Exception $exception)
-    {
-      if ($this->debug)
-        exception(
-          "Database | deleteDatabase()",
-          [
-            "message" => $exception->getMessage()
-          ]
-        );
-
-      return false;
-    }
-  }
 }
--- includes/database/create.php
+++ includes/database/create.php
@@ -4,8 +4,17 @@
 // Includes
 //==============================================================================
 
+// Constants
+require_once($_SERVER["DOCUMENT_ROOT"]."/includes/constants.php");
+
 // Database
 require_once($_SERVER["DOCUMENT_ROOT"]."/includes/classes/Database.php");
+
+//==============================================================================
+// Imports
+//==============================================================================
+
+use includes\classes\Database;
 
 //==============================================================================
 // Process
@@ -13,36 +22,40 @@ require_once($_SERVER["DOCUMENT_ROOT"]."/includes/classes/Database.php");
 
 try
 {
-  // Connection
-  $database = new Database();
+    // Connection
+    $database = new Database(
+        DB_HOSTNAME,
+        DB_USERNAME,
+        DB_PASSWORD
+    );
 
-  $database->deleteDatabase();
+    $database->deleteDatabase(DB_DATABASE);
 
-  $database->createDatabase();
+    $database->createDatabase(DB_DATABASE);
 
-  $database->selectDatabase();
+    $database->selectDatabase(DB_DATABASE);
 
-  $database->createTable(
-    "users",
-    [
-      "id" => "INT NOT NULL AUTO_INCREMENT PRIMARY KEY",
-      "username" => "VARCHAR(100) NOT NULL",
-      "password" => "VARCHAR(50) NOT NULL",
-    ]
-  );
+    $database->createTable(
+        "users",
+        [
+            "id" => "INT NOT NULL AUTO_INCREMENT PRIMARY KEY",
+            "username" => "VARCHAR(100) NOT NULL",
+            "password" => "VARCHAR(50) NOT NULL",
+        ]
+    );
 
-  $database->insertIntoTable(
-    "users",
-    [
-      "username" => "Jani",
-      "password" => "pw123"
-    ]
-  );
+    $database->insertIntoTable(
+        "users",
+        [
+            "username" => "Jani",
+            "password" => "pw123"
+        ]
+    );
 }
 catch (Exception $exception)
 {
-  display(
-    "Database create | Exception caught",
-    $exception->getMessage()
-  );
+    display(
+        "Database create | Exception caught",
+        $exception->getMessage()
+    );
 }
