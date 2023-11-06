f7e8e4c5f585accb7b8e436a0a0e9814e8225142
Holler János <hollerjanika@gmail.com>
2023-11-06 22:47:21 +0100

    Three new includes

---
+++ includes/header.php
@@ -0,0 +1,23 @@
+<?php
+
+//==============================================================================
+// Includes
+//==============================================================================
+
+// Constants
+require_once($_SERVER["DOCUMENT_ROOT"] . "/includes/constants.php");
+
+//==============================================================================
+// Display the header
+//==============================================================================
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>2FA - Two-factor authentication</title>
+        <meta charset="UTF-8"/>
+        <link rel="stylesheet" href="/assets/css/style.css?v=<?= RANDOM_VERSION ?>"/>
+        <script src="/assets/js/script.js?v=<?= RANDOM_VERSION ?>" defer></script>
+    </head>
+    <body>
