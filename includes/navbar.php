d0a7b66e4c36cc6e3f9c049648dfed45510d8bf1
Holler János <hollerjanika@gmail.com>
2023-11-06 23:24:02 +0100

    Navigation bar update
    Two difference navigation bars.
    One for logged-in users, and one for guests.

--- includes/navbar.php
+++ includes/navbar.php
@@ -4,8 +4,14 @@
 // Display the navigation bar
 //==============================================================================
 
+
 ?>
         <nav>
+<?php if (isset($_SESSION["login"]) && $_SESSION["login"]) { ?>
+            <a href="/clear.php">Logout</a>
+<?php } else { ?>
             <a href="/register.php">Register</a>
             <a href="/login.php">Login</a>
+<?php } ?>
+
         </nav>
